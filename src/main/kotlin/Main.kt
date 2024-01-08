1e55aa3a6a084c2ba62c95d1b8389ca0dd2024fd
ilopjim13 <ljivan@iesrafaelalberti.es>
2024-01-08 21:15:59 +0100

    terminado parte 1, mejorar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,41 @@
 fun main() {
-    var posx:Int
-    var posy:Int
-    var direccion:String
-    mover(intArrayOf(10, 5, -2))
+    var posicion: List<Int> = mover(10, 5, -2)
+    println("x: ${posicion[0]}, y: ${posicion[1]}, direction: ${orientacionRobot(posicion[2])}")
+    posicion = mover(0, 0, 0)
+    println("x: ${posicion[0]}, y: ${posicion[1]}, direction: ${orientacionRobot(posicion[2])}")
+    posicion = mover()
+    println("x: ${posicion[0]}, y: ${posicion[1]}, direction: ${orientacionRobot(posicion[2])}")
+    posicion = mover(-10, -5, 2)
+    println("x: ${posicion[0]}, y: ${posicion[1]}, direction: ${orientacionRobot(posicion[2])}")
+    posicion = mover(-10, -5, 2, 4, -8)
+    println("x: ${posicion[0]}, y: ${posicion[1]}, direction: ${orientacionRobot(posicion[2])}")
 }
 
-fun mover(lista: IntArray) :Array<Any> {
-
+/**
+ *
+ * @param lista List<Int> Lista de movimientos que debe realizar el robot.
+ * @return List<Int>
+ */
+fun mover(vararg movimientos: Int): List<Int> {
+    var posx = 0
+    var posy = 0
+    var direccion = 0
+    for (i in movimientos) {
+        when (direccion) {
+            0 -> posy += i
+            1 -> posx -= i
+            2 -> posy -= i
+            3 -> posx += i
+        }
+        if (direccion > 3) direccion = 0 else direccion++
+    }
+    return listOf(posx, posy, direccion)
 }
+
+fun orientacionRobot(direccion: Any) = when (direccion) {
+        0 -> "POSITIVEY"
+        1 -> "NEGATIVEX"
+        2 -> "NEGATIVEY"
+        3 -> "POSITIVEX"
+         else -> ""
+    }
